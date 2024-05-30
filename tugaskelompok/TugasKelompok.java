bc5b52785d56f59c2bbad5b0fa2edebc0b1a2d0b
SamuelEdowardo <164317671+SamuelEdowardo@users.noreply.github.com>
2024-05-30 22:47:19 +0700

    Add files via upload

---
+++ tugaskelompok/TugasKelompok.java
@@ -0,0 +1,12 @@
+
+package TugasKelompok;
+
+public class TugasKelompok {
+    
+    public static void main(String[] args){
+    Dashboard DashboardFrame = new Dashboard();
+    DashboardFrame.setVisible(true);
+    DashboardFrame.pack();
+    DashboardFrame.setLocationRelativeTo(null);
+    }
+}
